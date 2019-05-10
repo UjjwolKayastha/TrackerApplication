08a42e1ffa54517dc109b52757c9503feff3f5d2
Ujjwol Kayastha <uzol123@gmail.com>
2019-05-10 10:08:45 +0545

    Added UIs - SignUp, SignIn, MapsActivity

---
+++ app/src/main/java/kayastha/ujjwol/atrackerapp/SignUp.java
@@ -0,0 +1,15 @@
+package kayastha.ujjwol.atrackerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kayastha.ujjwol.atrackerapp.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
