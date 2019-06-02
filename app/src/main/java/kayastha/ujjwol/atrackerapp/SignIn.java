46a10726510df152fcf019b34dca7353d2952459
Ujjwol Kayastha <uzol123@gmail.com>
2019-06-02 18:49:27 +0545

    Integrated Signin with Google

--- app/src/main/java/kayastha/ujjwol/atrackerapp/SignIn.java
+++ app/src/main/java/kayastha/ujjwol/atrackerapp/SignIn.java
@@ -12,11 +12,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.model.Dash;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignIn extends AppCompatActivity {
 
@@ -27,9 +39,13 @@ public class SignIn extends AppCompatActivity {
 
     TextView actreg;
 
-
-
     private FirebaseAuth firebaseAuth;
+
+    SignInButton googleSignin;
+    private static final int RC_SIGN_IN = 9001;
+    GoogleApiClient mGooogleApiClient;
+
+
 
 
     @Override
@@ -51,7 +67,6 @@ public class SignIn extends AppCompatActivity {
         });
 
         login = findViewById(R.id.btn_signIn);
-
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,6 +95,88 @@ public class SignIn extends AppCompatActivity {
             }
         });
 
+        googleSignin = findViewById(R.id.signin_google);
+
+        signinGSO();
+
+        googleSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+
+    }
+
+    //google sign in Option
+    private void signinGSO(){
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGooogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGooogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            Log.d(TAG, googleSignInResult.getStatus().getStatusMessage());
+
+            if(googleSignInResult.isSuccess()){
+                GoogleSignInAccount account = googleSignInResult.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+
+
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed");
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            startActivity(new Intent(getApplicationContext(), DashBoard.class));
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(SignIn.this, "AUTHENTICATION FAILED", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     public boolean Validate(){
--- app/src/main/java/kayastha/ujjwol/atrackerapp/SignUp.java
+++ app/src/main/java/kayastha/ujjwol/atrackerapp/SignUp.java
@@ -17,17 +17,27 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import kayastha.ujjwol.atrackerapp.R;
 
 public class SignUp extends AppCompatActivity {
 
+    CircleImageView profile_image;
     EditText name, email, password, cpassword;
     Button signup;
 
     TextView actlogin;
     //Firebase
-    private FirebaseAuth firebaseAuth;
+    FirebaseAuth firebaseAuth;
+    DatabaseReference reference;
+
+
+    String userID;
 
 
     @Override
@@ -39,7 +49,7 @@ public class SignUp extends AppCompatActivity {
         firebaseAuth  = FirebaseAuth.getInstance();
 
 
-
+        profile_image = findViewById(R.id.profile_image);
         name = findViewById(R.id.signup_name);
         email = findViewById(R.id.signup_email);
         password = findViewById(R.id.signup_password);
@@ -65,12 +75,23 @@ public class SignUp extends AppCompatActivity {
 
                 if(Validate()){
 
-                    firebaseAuth.createUserWithEmailAndPassword(inemail,inpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    firebaseAuth.createUserWithEmailAndPassword(inemail,inpassword)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
+                                userID = firebaseAuth.getCurrentUser().getUid();
+
+                                reference = FirebaseDatabase.getInstance().getReference("Users").child(userID);
+
+//                                HashMap<String, String> hashMap = new HashMap<>();
+//                                hashMap.put("id", userID);
+//                                hashMap.put("id", userID);
+//                                hashMap.put("id", userID);
+
                                 Toast.makeText(SignUp.this, "REGISTRATION SUCCESSFUL", Toast.LENGTH_SHORT).show();
                                 startActivity(new Intent(getApplicationContext(), SignIn.class));
+
 
                             }else {
                                 // If sign in fails, display a message to the user.
